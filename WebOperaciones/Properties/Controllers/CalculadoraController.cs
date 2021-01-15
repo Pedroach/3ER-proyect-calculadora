1b83c8d5a213fdb46235a8ef79d290eb2bf11ed9
Pedro <a2019114887@estudiantes.edu.bo>
2021-01-14 21:19:02 -0400

    commit de resta

--- WebOperaciones/Properties/Controllers/CalculadoraController.cs
+++ WebOperaciones/Properties/Controllers/CalculadoraController.cs
@@ -11,14 +11,52 @@ namespace WebOperaciones.Properties.Controllers
     public class CalculadoraController : Controller
     {
         [HttpGet]
+        [Route("suma")]
         public int suma(int num1, int num2)
         {
             return num1 + num2;
         }
         [HttpPost]
+        [Route("suma")]
         public int sumar([FromHeader] int num1, [FromHeader] int num2)
         {
             return num1 + num2;
         }
+        [HttpGet]
+        [Route("resta")]
+        public int resta(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        [HttpPost]
+        [Route("resta")]
+        public int restar([FromHeader] int num1, [FromHeader] int num2)
+        {
+            return num1 - num2;
+        }
+        /*[HttpGet]
+        [Route("dividir")]
+        public int dividir(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+        [HttpPost]
+        [Route("dividir")]
+        public int divide([FromHeader] int num1, [FromHeader] int num2)
+        {
+            return num1 / num2;
+        }
+        [HttpGet]
+        [Route("multipliacion")]
+        public int multiplicacion(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        [HttpPost]
+        [Route("multipliacion")]
+        public int multi([FromHeader] int num1, [FromHeader] int num2)
+        {
+            return num1 * num2;
+        }*/
     }
 }
